8ded8c2b210859e4270d53bc2cc03c1f379f7980
RobertObi <robobi97@googlemail.com>
2021-03-16 15:43:05 -0500

    commit tree 5f15a83418288d9387b8dcd8d3404c828c027f6a

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        user_input = params[:user_phrase]
+
+
+        # erb :user_input
+
+    end 
+
+
+
 end
+
+#renders the instructions 
